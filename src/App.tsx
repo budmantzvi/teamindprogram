f4f8c6d4cdea6fb383faf57daf8019aba89d3d2c
Tzvi Budman <budmantzvi@gmail.com>
2026-09-16 01:42:32 +0300

    refactor: update asset URLs to use www subdomain
    Standardize all favicon and manifest links to use the www subdomain for consistent resource loading.

--- src/App.tsx
+++ src/App.tsx
@@ -111,14 +111,14 @@ function LanguageHandler() {
       <link rel="alternate" hrefLang="x-default" href={enUrl} />
 
       {/* Universal Icons */}
-      <link rel="shortcut icon" href="https://teamindprogram.com/favicon.ico" type="image/x-icon" />
-      <link rel="icon" href="https://teamindprogram.com/favicon.ico" type="image/x-icon" />
-      <link rel="icon" type="image/png" sizes="16x16" href="https://teamindprogram.com/favicon-16x16.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="https://teamindprogram.com/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="https://teamindprogram.com/android-chrome-192x192.png" />
-      <link rel="icon" type="image/png" sizes="512x512" href="https://teamindprogram.com/android-chrome-512x512.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="https://teamindprogram.com/apple-touch-icon.png" />
-      <link rel="manifest" href="https://teamindprogram.com/manifest.json" />
+      <link rel="shortcut icon" href="https://www.teamindprogram.com/favicon.ico" type="image/x-icon" />
+      <link rel="icon" href="https://www.teamindprogram.com/favicon.ico" type="image/x-icon" />
+      <link rel="icon" type="image/png" sizes="16x16" href="https://www.teamindprogram.com/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="https://www.teamindprogram.com/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="https://www.teamindprogram.com/android-chrome-192x192.png" />
+      <link rel="icon" type="image/png" sizes="512x512" href="https://www.teamindprogram.com/android-chrome-512x512.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="https://www.teamindprogram.com/apple-touch-icon.png" />
+      <link rel="manifest" href="https://www.teamindprogram.com/manifest.json" />
 
       {/* Open Graph */}
       <meta property="og:type" content="website" />
