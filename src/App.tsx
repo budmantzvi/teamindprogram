13baf6c0c6a5686fb454eaece9264da72b598d80
Tzvi Budman <budmantzvi@gmail.com>
2026-09-16 13:33:33 +0300

    refactor: update favicon and icon asset paths
    Change asset references to root-relative paths and update image files to ensure consistent branding and browser compatibility.

--- src/App.tsx
+++ src/App.tsx
@@ -111,14 +111,15 @@ function LanguageHandler() {
       <link rel="alternate" hrefLang="x-default" href={enUrl} />
 
       {/* Universal Icons */}
-      <link rel="shortcut icon" href="https://www.teamindprogram.com/favicon.ico" type="image/x-icon" />
-      <link rel="icon" href="https://www.teamindprogram.com/favicon.ico" type="image/x-icon" />
-      <link rel="icon" type="image/png" sizes="16x16" href="https://www.teamindprogram.com/favicon-16x16.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="https://www.teamindprogram.com/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="https://www.teamindprogram.com/android-chrome-192x192.png" />
-      <link rel="icon" type="image/png" sizes="512x512" href="https://www.teamindprogram.com/android-chrome-512x512.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="https://www.teamindprogram.com/apple-touch-icon.png" />
-      <link rel="manifest" href="https://www.teamindprogram.com/manifest.json" />
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+      <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/manifest.json" />
 
       {/* Open Graph */}
       <meta property="og:type" content="website" />
